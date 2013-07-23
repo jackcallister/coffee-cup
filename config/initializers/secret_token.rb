64952b02c243e725daded42ef795e0056796af2e
Jack Callister <jarsbe@gmail.com>
2013-07-23 22:12:10 +1200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeCup::Application.config.secret_key_base = 'e3c76c4b96f015a3cd70cd2bdf4cc749d40b09ee24288559a8c0b61a1a93f276b774731d9fc4d3f19ecbde96c723154168572d2e785e00432e534fe804065615'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeCup::Application.config.session_store :cookie_store, key: '_coffee-cup_session'
